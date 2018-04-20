a86684302df6a9ae03b7618316175c5008a0d8bf
xiongben <wangdongkf@163.com>
2018-04-20 15:49:56 +0800

    修复短信和服务类

--- src/main/java/com/gws/controllers/api/SmsController.java
+++ src/main/java/com/gws/controllers/api/SmsController.java
@@ -8,6 +8,7 @@ import com.gws.services.sms.SmsService;
 import com.gws.utils.json.JsonParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
